4d3c7388c7352ce76884a7fc9c89dd86aeb79286
lubanas83 <58112021+lubanas83@users.noreply.github.com>
2019-11-23 15:33:25 +0100

    Update HElloWorld.java

--- src/HElloWorld.java
+++ src/HElloWorld.java
@@ -2,5 +2,7 @@ public class HElloWorld {
 
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
+        System.out.println("HelloWorld2!");
+        System.out.println("HelloWorld3!");
     }
 }
